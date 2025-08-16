0fcd7ab36b1583b3a1fff29f7da1dc651743b1aa
Doe Sparkle <thesparkledoe@gmail.com>
2025-08-16 04:46:06 -0700

    Short-circuits ENS resolver requests for Yominet
    Yominet does not support ENS, so this prevents resolver requests to avoid unnecessary errors. This is intended for dev builds hosted on ENS domains.

--- packages/client/src/engine/providers/types.ts
+++ packages/client/src/engine/providers/types.ts
@@ -29,6 +29,12 @@ export class MUDJsonRpcProvider extends JsonRpcProvider {
   constructor(url: string | ConnectionInfo | undefined, network: Networkish) {
     super(url, network);
   }
+
+  // Yominet does not support ENS, so this short-circuits resolver requests to avoid noisy errors.
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  async _getResolver(_name: string): Promise<any> {
+    return null;
+  }
   async detectNetwork(): Promise<Network> {
     const network = this.network;
     if (network == null) {
@@ -41,6 +47,10 @@ export class MUDJsonRpcProvider extends JsonRpcProvider {
 export class MUDJsonRpcBatchProvider extends JsonRpcBatchProvider {
   constructor(url?: string | ConnectionInfo | undefined, network?: Networkish | undefined) {
     super(url, network);
+  }
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  async _getResolver(_name: string): Promise<any> {
+    return null;
   }
   async detectNetwork(): Promise<Network> {
     const network = this.network;
