1276d113e2822b49166f26852f626029badb1795
Doe Sparkle <160281365+deeterbleater@users.noreply.github.com>
2025-08-18 06:14:50 -0700

    removed unneeded files from contracts folder

--- packages/contracts/vercel.sh
+++
@@ -1,9 +0,0 @@
-if [[ $MODE == "production" ]]; then 
-  pnpm -F client build:prod
-elif [[ $MODE == "staging" ]]; then
-  pnpm -F client build:prev
-elif [[ $MODE == "testing" ]]; then
-  pnpm -F client build:test
-else
-  pnpm -F client build:prod
-fi
